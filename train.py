62ae7008dcdfec10c06356e8300bd0c8a8071bcd
L0SG <tkdrlf9202@gmail.com>
2017-08-28 15:10:48 +0900

    validation & logging

--- train.py
+++ train.py
@@ -1,57 +1,45 @@
 from lib import utils
 from lib import unet
+from lib import datahandler
 import os.path
+import sys
 import torch
 from torch.autograd import Variable
 from torch.utils.data import TensorDataset, DataLoader
+from sklearn.model_selection import train_test_split
 import numpy as np
 import torchvision
 import h5py
 
-# set GPU ID to use
+# GPU ID to use
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
-# path for preprocessed data
+# hyperparameters
+learning_rate = 1e-4
+momentum = 0.8
+weight_decay = 0.0005
+batch_size = 8
+
+# path for original & preprocessed data
 preprocessed_data_path = '/home/vision/tkdrlf9202/Datasets/liver_preprocessed/liver_data.h5'
+data_path = '/home/vision/tkdrlf9202/Datasets/liver'
 
-# check if the preprocessed dataset exists
-if os.path.isfile(preprocessed_data_path):
-    # load the preprocessed dataset dump
-    print('loading preprocessed dataset...')
-    with h5py.File(preprocessed_data_path, 'r') as dataset_h5:
-        ct_flattened = dataset_h5['ct'][:]
-        mask_flattened = dataset_h5['mask'][:]
-        dataset_h5.close()
-else:
-    # load the liver dataset
-    print('reading dicom and mask data...')
-    list_ct, list_mask = utils.load_liver_seg_dataset(data_path='/home/vision/tkdrlf9202/Datasets/liver',
-                                                      num_data_to_load=None)
-    # preprocess the dataset
-    print('preprocessing dataset...')
-    list_ct_preprocessed, list_mask_preprocessed = utils.preprocess_liver_dataset(list_ct, list_mask)
-
-    # flatten the data from [subjects, width, height, depth] to [subject*depth(=datasize), 1(channel), width, height]
-    ct_flattened = np.concatenate(list_ct_preprocessed, axis=2).transpose([2, 0, 1])
-    ct_flattened = np.expand_dims(ct_flattened, axis=1)
-    mask_flattened = np.concatenate(list_mask_preprocessed, axis=2).transpose([2, 0, 1])
-    mask_flattened = np.expand_dims(mask_flattened, axis=1)
-
-    # dump the preprocessed dataset for later use
-    print('dumping dataset...')
-    with h5py.File(preprocessed_data_path, 'w') as dump_h5:
-        dump_h5.create_dataset('ct', data=ct_flattened)
-        dump_h5.create_dataset('mask', data=mask_flattened)
-        dump_h5.close()
+# load preprocessed dataset
+ct_flattened, mask_flattened = datahandler.load_liver_dataset(preprocessed_data_path, data_path)
+ct_train, ct_valid, mask_train, mask_valid = train_test_split(ct_flattened, mask_flattened, test_size=0.1)
 
 # wrap the data to pytorch tensordataset and construct dataloader
 print('constructing dataloader...')
-liver_tensor_dataset = TensorDataset(data_tensor=torch.FloatTensor(ct_flattened),
-                                     target_tensor=torch.LongTensor(mask_flattened.astype(np.int64)))
-liver_dataloader = DataLoader(dataset=liver_tensor_dataset, batch_size=8,
-                              shuffle=True, drop_last=True)
+liver_tensor_dataset_train = TensorDataset(data_tensor=torch.FloatTensor(ct_train),
+                                           target_tensor=torch.LongTensor(mask_train.astype(np.int64)))
+liver_tensor_dataset_valid = TensorDataset(data_tensor=torch.FloatTensor(ct_valid),
+                                           target_tensor=torch.LongTensor(mask_valid.astype(np.int64)))
 
+liver_dataloader_train = DataLoader(dataset=liver_tensor_dataset_train, batch_size=batch_size,
+                                    shuffle=True, drop_last=True)
+liver_dataloader_valid = DataLoader(dataset=liver_tensor_dataset_valid, batch_size=batch_size,
+                                    shuffle=True, drop_last=True)
 
 # define unet for binary pixel-wise segmentation
 print('loading the model...')
@@ -62,18 +50,29 @@ model_unet = torch.nn.DataParallel(
 # define the optimizer
 # optimizer = torch.optim.Adam(params=model_unet.parameters(), lr=1e-5)
 # same hyper params with cascaded FCN
-optimizer = torch.optim.SGD(params=model_unet.parameters(), lr=1e-4, momentum=0.8,
-                            weight_decay=0.0005, nesterov=True)
+optimizer = torch.optim.SGD(params=model_unet.parameters(), lr=learning_rate,
+                            momentum=momentum, weight_decay=weight_decay, nesterov=True)
 
-# train the model
-epochs = 1000
+# path for results & logs
+results_path = 'results_'+'_'+str(learning_rate)+'_'+str(momentum)+'_'+str(weight_decay)
+if not os.path.exists(results_path):
+    os.makedirs(results_path)
+# make log file
+logger_train = open(os.path.join(results_path, 'train_log.txt'), 'w')
+logger_valid = open(os.path.join(results_path, 'valid_log.txt'), 'w')
+# path for trained model parameters
+model_param_path = 'model_unet_parameters'
+
+# train and validate the model
+epochs = 100
 for epoch in range(epochs):
-    if not os.path.exists('train_samples'):
-        os.makedirs('train_samples')
-    samples_save_path = os.path.join('train_samples', 'epoch_'+str(epoch))
+    samples_save_path = os.path.join(results_path, 'epoch_'+str(epoch))
     if not os.path.exists(samples_save_path):
         os.makedirs(samples_save_path)
-    for idx, (inputs, targets) in enumerate(liver_dataloader):
+
+    # training phase
+    running_loss = 0.
+    for idx, (inputs, targets) in enumerate(liver_dataloader_train):
         # calculate class weight
         # currently hard-coding, needs to be more generic
         num_pixels_background = torch.numel(targets[targets == 0])
@@ -103,11 +102,18 @@ for epoch in range(epochs):
         model_unet.zero_grad()
         loss.backward()
         optimizer.step()
+        # add step loss to running loss
+        running_loss += loss.data[0]
 
-        # print current epoch, step and loss
-        if (idx + 1) % 1 == 0:
-            print('epoch ' + str(epoch) + ' step ' + str(idx+1) + ' loss ' + str(loss.data[0]))
-        # save inputs, targets and sigmoid outputs to image
+        # print current epoch, step and avg.loss ; then save to logger
+        if (idx + 1) % 10 == 0:
+            running_loss /= 10
+            logging_data = 'epoch ' + str(epoch) + ' step ' + str(idx+1) + ' loss ' + str(running_loss)
+            print(logging_data)
+            logger_train.write(logging_data + '\n')
+            running_loss = 0.
+
+        # save inputs, targets and softmax outputs to image
         if (idx + 1) % 50 == 0:
             torchvision.utils.save_image(inputs.data,
                                          os.path.join(samples_save_path, 'input_'+str(idx)+'.jpg'))
@@ -117,6 +123,38 @@ for epoch in range(epochs):
             torchvision.utils.save_image(torch.unsqueeze(softmax.data[:, 1, :, :], dim=1),
                                          os.path.join(samples_save_path, 'softmax_'+str(idx)+'.jpg'))
 
-# save the model
+    # validation phase
+    print('epoch finished, running validation phase...')
+    valid_loss = 0.
+    for idx_valid, (inputs, targets) in enumerate(liver_dataloader_valid):
+        # same as training phase
+        num_pixels_background = torch.numel(targets[targets == 0])
+        num_pixels_foreground = torch.numel(targets[targets == 1])
+        class_weight_background = float(num_pixels_foreground) / (num_pixels_background + num_pixels_background)
+        class_weight_foreground = 1 - class_weight_background
+        class_weight = torch.FloatTensor([class_weight_background, class_weight_foreground]).cuda()
 
-print ('testtest')
+        inputs = Variable(inputs).cuda()
+        targets = Variable(targets).cuda()
+
+        targets = torch.squeeze(targets, dim=1)
+
+        softmax = model_unet(inputs)
+
+        loss_nll2d = torch.nn.NLLLoss2d(weight=class_weight)
+        loss = loss_nll2d(torch.log(softmax), targets)
+        # add to total validation loss
+        valid_loss += loss.data[0]
+    # divide with total step index
+    valid_loss = valid_loss / idx_valid
+    # print and log
+    print('validation loss: ' + str(valid_loss))
+    logger_valid.write('validation loss: ' + str(valid_loss) + '\n')
+
+    # save the model
+    torch.save(model_unet.state_dict(), os.path.join(results_path, model_param_path+'_epoch_'+str(epoch)))
+    print('model saved')
+
+# save the model
+torch.save(model_unet.state_dict(), os.path.join(results_path, model_param_path+'_final'))
+print('model saved')
