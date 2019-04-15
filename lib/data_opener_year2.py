27d27e63770f1bf922ccf9df1c80d46ad3bc0eab
tkdrlf9202 <tkdrlf9202@gmail.com>
2019-04-15 12:45:09 +0900

    year2 1810 dataset label processing

--- lib/data_opener_year2.py
+++ lib/data_opener_year2.py
@@ -18,6 +18,7 @@ for subject in glob.glob(os.path.join(data_path, '*')):
                       os.path.join(subject, basename_new))
 """
 
+
 def apply_window(img, window_width, window_level):
     """
     apply CT windowing function in radiology terms
@@ -29,12 +30,12 @@ def apply_window(img, window_width, window_level):
     :param window_level: window level
     :return:
     """
-    # if np.amin(img) == 0:
-    #     print("WARNING: This CT image has minimum CT HU of 0. Double check if the windowing function is right")
-    #     print("WARNING: manually adjusting HU to have minimum value of -1024...")
-    #     img = np.subtract(img, 1024)
-    #
-    # img[img > 1200] = 0
+    if np.amin(img) == 0:
+        print("WARNING: This CT image has minimum CT HU of 0. Double check if the windowing function is right")
+        print("WARNING: manually adjusting HU to have minimum value of -1024...")
+        img = np.subtract(img, 1024)
+
+    img[img > 1200] = 0
 
     upper_grey = window_level + (window_width / 2.)
     lower_grey = window_level - (window_width / 2.)
@@ -60,8 +61,13 @@ def read_liver_seg_masks_raw_year2(masks_dirname, img_shape):
     order = [2, 0, 1]
     shape_raw = shape_raw[order]
     num_slice = rawfile.shape[0] / shape_raw[1] / shape_raw[2]
+    assert float(int(num_slice)) == num_slice
+    num_slice = int(num_slice)
     print(os.path.basename(masks_dirname) + ' slices raw vs dicom: ' + str(num_slice) + ' ' + str(shape_raw[0]))
 
+    if num_slice != shape_raw[0]:
+        print("WARNING: {} mask slice {} does not match CT image {}".format(os.path.basename(masks_dirname), num_slice,
+                                                                            shape_raw[0]))
     # TODO: this is temporary for just seeing the raw dataset
     shape_raw[0] = num_slice
 
@@ -111,25 +117,27 @@ def load_liver_seg_dataset_year2(data_path, num_data_to_load, window_width, wind
         dicom_image_before = np.array(dicom_image_before)
         dicom_image_after = np.array(dicom_image_after)
         assert dicom_image_before.shape == dicom_image_after.shape
-        print(os.path.basename(path_subject) + " min_prect, max_prect, min_ct, max_ct: " + str(np.amin(dicom_image_before)) + ' ' + str(np.amax(dicom_image_before)) + ' ' +
+        print(os.path.basename(path_subject) + " min_prect, max_prect, min_ct, max_ct: " + str(
+            np.amin(dicom_image_before)) + ' ' + str(np.amax(dicom_image_before)) + ' ' +
               str(np.amin(dicom_image_after)) + ' ' + str(np.amax(dicom_image_after)))
 
         # apply HU windowing to the dicom images
         dicom_image_before = apply_window(dicom_image_before, window_width, window_level)
         dicom_image_after = apply_window(dicom_image_after, window_width, window_level)
 
-
-        """ TODO: 1810 dataset has no labels
+        # TODO: 1810 dataset has D labels
         # load mask image of the subject, which needs shape information
         dicom_shape = dicom_image_after.shape[1:]
-        mask_path = os.path.join(path_subject, str(dir_name)+'.raw')
+        mask_path = os.path.join(path_subject, str(dir_name) + '.raw')
+        # FIXME: 1810 hard-wire
+        mask_path = mask_path.replace(".raw", "_D.raw")
         mask_image = read_liver_seg_masks_raw_year2(mask_path, img_shape=dicom_shape)
 
         # print unique elements of mask tensor
         # for binary segmentation, it should only contain 0 or 1
         # if the tensor contains other number, data structure is not right
         mask_unique_elems = np.unique(mask_image)
-        ground_truth_reference = [0, 1] # to assert binary data
+        ground_truth_reference = [0, 1]  # to assert binary data
         # print('unique elements of mask image: ' + str(mask_unique_elems))
         if sorted(mask_unique_elems) != sorted(ground_truth_reference):
             print('incorrect mask label detected, forcing incorrect labels to zero...')
@@ -141,40 +149,51 @@ def load_liver_seg_dataset_year2(data_path, num_data_to_load, window_width, wind
         mask_unique_elems = np.unique(mask_image)
         assert len(mask_unique_elems) == len(ground_truth_reference)
         assert sorted(mask_unique_elems) == sorted(ground_truth_reference)
-        """
+
         # append to list
         list_prect.append(dicom_image_before)
         list_ct.append(dicom_image_after)
         # list_mask.append(mask_image)
 
-
         # DEBUG ONLY: save dicom & mask images for given directory
         # for checking if the label data has any artifacts
         # save mask data as image
-        mask_image_save_path = '/home/tkdrlf9202/Datasets/liver_mask_image_year2_1810_ww{}_wl{}'.\
+        mask_image_save_path = '/home/tkdrlf9202/Datasets/liver_mask_image_year2_1810_ww{}_wl{}'. \
             format(window_width, window_level)
         if not os.path.exists(os.path.join(mask_image_save_path, str(dir_name))):
             os.makedirs(os.path.join(mask_image_save_path, str(dir_name)))
 
-        # take the minimum one: for outputting concatenated image of prect, ct, and mask
-        """z_min = min(mask_image.shape[2], dicom_image_before.shape[3])"""
-        z_min = dicom_image_before.shape[3]
+        # take the maximum one: for outputting concatenated image
+        z_max = np.max([dicom_image_before.shape[3], dicom_image_after.shape[3], mask_image.shape[2]])
 
-        for idx in range(z_min):
-            cat_slice_save_path = os.path.join(mask_image_save_path, str(dir_name), str(idx)+'.jpg')
-            cat_slice = np.zeros([512*2, 512*4])
+        for idx in range(z_max):
+            cat_slice_save_path = os.path.join(mask_image_save_path, str(dir_name), str(idx) + '.jpg')
+            cat_slice = np.zeros([512 * 2, 512 * 5])
 
-            # mask_slice = np.copy(mask_image[:, :, idx])
-            # # if 0, stay 0; if 1, convert to 255
-            # mask_slice[mask_slice == 1] = 255
-            dicom_slice_before = np.copy(dicom_image_before[:, :, :, idx])
-            dicom_slice_after = np.copy(dicom_image_after[:, :, :, idx])
+
+            try:
+                dicom_slice_before = np.copy(dicom_image_before[:, :, :, idx])
+                dicom_slice_after = np.copy(dicom_image_after[:, :, :, idx])
+            except IndexError:
+                # ct image not found (since ct vs mask is not aligned), replace with noise
+                dicom_slice_before = np.random.randint(0, 255, size=(4, 512, 512))
+                dicom_slice_after = np.random.randint(0, 255, size=(4, 512, 512))
+
             dicom_slice_before_normalized = (dicom_slice_before - float(np.amin(dicom_slice_before))) / (
                     float(np.amax(dicom_slice_before)) - float(np.amin(dicom_slice_before)))
             dicom_slice_after_normalized = (dicom_slice_after - float(np.amin(dicom_slice_after))) / (
-                        float(np.amax(dicom_slice_after)) - float(np.amin(dicom_slice_after)))
+                    float(np.amax(dicom_slice_after)) - float(np.amin(dicom_slice_after)))
             dicom_slice_before_normalized *= 255
             dicom_slice_after_normalized *= 255
+
+            try:
+                mask_slice = np.copy(mask_image[:, :, idx])
+                # if 0, stay 0; if 1, convert to 255
+                mask_slice[mask_slice == 1] = 255
+            except IndexError:
+                # mask slice not found (ct vs mask not aligned), replace with noise
+                mask_slice = np.random.randint(0, 255, size=(512, 512))
+
             """
             cat_slice[:, 0:512] = dicom_slice_before_normalized
             cat_slice[:, 512:512*2] = dicom_slice_after_normalized
@@ -184,10 +203,11 @@ def load_liver_seg_dataset_year2(data_path, num_data_to_load, window_width, wind
             dicom_slice_before_normalized = np.split(dicom_slice_before_normalized, 4, axis=0)
             dicom_slice_after_normalized = np.split(dicom_slice_after_normalized, 4, axis=0)
             for i in range(4):
-                cat_slice[:512, 512*(i):512*(i+1)] = dicom_slice_before_normalized[i]
-                cat_slice[512:, 512*(i):512*(i+1)] = dicom_slice_after_normalized[i]
+                cat_slice[:512, 512 * (i):512 * (i + 1)] = dicom_slice_before_normalized[i]
+                cat_slice[512:, 512 * (i):512 * (i + 1)] = dicom_slice_after_normalized[i]
+            # display mask
+            cat_slice[512:, 512 * 4:] = mask_slice
             scipy.misc.imsave(cat_slice_save_path, cat_slice)
-
 
         # # traverse through slices
         # for idx in range(mask_image.shape[2]):
@@ -224,8 +244,6 @@ def load_liver_seg_dataset_year2(data_path, num_data_to_load, window_width, wind
 
 
 data_path = "/home/tkdrlf9202/Datasets/liver_year2_dataset_1810"
-prect, ct, mask = load_liver_seg_dataset_year2(data_path, None, window_width=2300, window_level=1750)
-
-
+prect, ct, mask = load_liver_seg_dataset_year2(data_path, None, window_width=400, window_level=50)
 
 print("done")
